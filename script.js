e6eb8a1465a285e028682a06b0bdea74135fcad2
anwarul1 <anwarjewel007@gmail.com>
2022-09-14 09:12:00 +0600

    implement highscore to the game

--- script.js
+++ script.js
@@ -15,10 +15,11 @@ console.log(document.querySelector('.highscore').textContent);
 */
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+  // console.log(guess, typeof guess);
 
   // when there is no input
   if (!guess) {
@@ -33,6 +34,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // when guess is too high
   } else if (guess > secretNumber) {
